88c84572cc6c743aeab5c0af8500780ed4580de6
Ara Abcarians <itsme@ara-abcarians.com>
2011-05-03 11:13:34 -0700

    v1.5 restructured code, switched to use ID for aToolTip and class name for easier theme creation, added min and packed versions

--- js/jquery.atooltip.js
+++ js/jquery.atooltip.js
@@ -1,34 +1,39 @@
 /*
 	jQuery Version:				jQuery 1.3.2+
-	Plugin Name:				aToolTip V 1.2
+	Plugin Name:				aToolTip V 1.5
 	Plugin by: 					Ara Abcarians: http://ara-abcarians.com
 	License:					aToolTip is licensed under a Creative Commons Attribution 3.0 Unported License
 								Read more about this license at --> http://creativecommons.org/licenses/by/3.0/			
 */
 (function($) {
     $.fn.aToolTip = function(options) {
-    
-    	// setup default settings
+    	/**
+    		setup default settings
+    	*/
     	var defaults = {
-    		clickIt: false,
+    		// no need to change/override
     		closeTipBtn: 'aToolTipCloseBtn',
+    		toolTipId: 'aToolTip',
+    		// ok to override
     		fixed: false,
-    		inSpeed: 400,
+    		clickIt: false,
+    		inSpeed: 200,
     		outSpeed: 100,
     		tipContent: '',
-    		toolTipClass: 'aToolTip',
+    		toolTipClass: 'defaultTheme',
     		xOffset: 5,
     		yOffset: 5,
     		onShow: null,
     		onHide: null
     	},
-    
     	// This makes it so the users custom options overrides the default ones
     	settings = $.extend({}, defaults, options);
     
 		return this.each(function() {
 			var obj = $(this);
-			// Decide weather to use a title attr as the tooltip content
+			/**
+				Decide weather to use a title attr as the tooltip content
+			*/
 			if(obj.attr('title')){
 				// set the tooltip content/text to be the obj title attribute
 				var tipContent = obj.attr('title');	 
@@ -37,89 +42,89 @@
 				var tipContent = settings.tipContent;
 			}
 			
-			// check if obj has a title attribute and if click feature is off
+			/**
+				Build the markup for aToolTip
+			*/
+			var buildaToolTip = function(){
+				$('body').append("<div id='"+settings.toolTipId+"' class='"+settings.toolTipClass+"'><p class='aToolTipContent'>"+tipContent+"</p></div>");
+				
+				if(tipContent && settings.clickIt){
+					$('#'+settings.toolTipId+' p.aToolTipContent')
+					.append("<a id='"+settings.closeTipBtn+"' href='#' alt='close'>close</a>");
+				}
+			},
+			/**
+				Position aToolTip
+			*/
+			positionaToolTip = function(){
+				$('#'+settings.toolTipId).css({
+					top: (obj.offset().top - $('#'+settings.toolTipId).outerHeight() - settings.yOffset) + 'px',
+					left: (obj.offset().left + obj.outerWidth() + settings.xOffset) + 'px'
+				})
+				.stop().fadeIn(settings.inSpeed, function(){
+					if ($.isFunction(settings.onShow)){
+						settings.onShow(obj);
+					}
+				});				
+			},
+			/**
+				Remove aToolTip
+			*/
+			removeaToolTip = function(){
+				// Fade out
+				$('#'+settings.toolTipId).stop().fadeOut(settings.outSpeed, function(){
+				    $(this).remove();
+				    if($.isFunction(settings.onHide)){
+						settings.onHide(obj);
+					}
+				});				
+			};
+			
+			/**
+				Decide what kind of tooltips to display
+			*/
+			// Regular aToolTip
 			if(tipContent && !settings.clickIt){	
 				// Activate on hover	
-				obj.hover(function(el){
+				obj.hover(function(){
 					// remove already existing tooltip
-					$('.' + settings.toolTipClass).remove();
-					
+					$('#'+settings.toolTipId).remove();
 					obj.attr({title: ''});
-											  
-					$('body').append("<div class='"+ settings.toolTipClass +"'><p class='aToolTipContent'>"+ tipContent +"</p></div>");
-					$('.' + settings.toolTipClass).css({
-						position: 'absolute',
-						display: 'none',
-						zIndex: '50000',
-						top: (obj.offset().top - $('.' + settings.toolTipClass).outerHeight() - settings.yOffset) + 'px',
-						left: (obj.offset().left + obj.outerWidth() + settings.xOffset) + 'px'
-					})
-					.stop().fadeIn(settings.inSpeed, function(){
-						if ($.isFunction(settings.onShow)){
-							settings.onShow(obj);
-						}
-					});	
-			    },
-				function(){ 
-					// Fade out
-					$('.' + settings.toolTipClass).stop().fadeOut(settings.outSpeed, function(){
-					    $(this).remove();
-					    if ($.isFunction(settings.onHide)){
-							settings.onHide(obj);
-						}
-					});
+					buildaToolTip();
+					positionaToolTip();
+			    }, function(){ 
+					removeaToolTip();
 			    });	
-		    }
+		    } 		    
 		    
-		    // Follow mouse if fixed is false and click is false
-		    if(!settings.fixed && !settings.clickIt){
-				obj.mousemove(function(el){
-					$('.' + settings.toolTipClass).css({
-						top: (el.pageY - $('.' + settings.toolTipClass).outerHeight() - settings.yOffset),
-						left: (el.pageX + settings.xOffset)
-					});
-				});			
-			} 		    
-		    
-		    // check if click feature is enabled
+		    // Click activated aToolTip
 		    if(tipContent && settings.clickIt){
 				// Activate on click	
 				obj.click(function(el){
 					// remove already existing tooltip
-					$('.' + settings.toolTipClass).remove();
-					
+					$('#'+settings.toolTipId).remove();
 					obj.attr({title: ''});
-											  
-					$('body').append("<div class='"+ settings.toolTipClass +"'><p class='aToolTipContent'>"+ tipContent +"</p></div>");
-					$('.' + settings.toolTipClass + ' p.aToolTipContent').append("<a class='"+ settings.closeTipBtn +"' href='#' alt='close'>close</a>");
-					$('.' + settings.toolTipClass).css({
-						position: 'absolute',
-						display: 'none',
-						zIndex: '50000',
-						top: (obj.offset().top - $('.' + settings.toolTipClass).outerHeight() - settings.yOffset) + 'px',
-						left: (obj.offset().left + obj.outerWidth() + settings.xOffset) + 'px'
-					})
-					.fadeIn(settings.inSpeed, function(){
-						if ($.isFunction(settings.onShow)){
-							settings.onShow(obj);
-						}
-					});	
+					buildaToolTip();
+					positionaToolTip();
 					// Click to close tooltip
-					$('.' + settings.closeTipBtn).click(function(){
-						$('.' + settings.toolTipClass).fadeOut(settings.outSpeed, function(){
-							$(this).remove();
-							if ($.isFunction(settings.onHide)){
-								settings.onHide(obj);
-							}
-						});
+					$('#'+settings.closeTipBtn).click(function(){
+						removeaToolTip();
 						return false;
 					});		 
 					return false;			
 			    });
 		    }
+		    
+		    // Follow mouse if enabled
+		    if(!settings.fixed && !settings.clickIt){
+				obj.mousemove(function(el){
+					$('#'+settings.toolTipId).css({
+						top: (el.pageY - $('#'+settings.toolTipId).outerHeight() - settings.yOffset),
+						left: (el.pageX + settings.xOffset)
+					});
+				});			
+			}		    
 		  
 		}); // END: return this
-		
-		// return this;
     };
 })(jQuery);
